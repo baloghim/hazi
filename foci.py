bbb6acc3dbd30876a6f8eb7fb15cdc0e81d0c2a3
baloghim <balogh.imre.mt@gmail.com>
2022-05-12 12:59:16 +0200

    4. feladatig

---
+++ foci.py
@@ -0,0 +1,59 @@
+import random
+golokA= []
+golokB = []
+
+print("1. feladat - Eredmények:")
+print("")
+for i in range(5):
+    golA = random.randint(0, 5)
+    golB = random.randint(0, 5)
+    print(str(i + 1) + ". forduló: " + str(golA) + ":" + str(golB))
+    golokA.append(golA)
+    golokB.append(golB)
+
+print("")
+print("2. feladat")
+osszesen=sum(golokA) + sum(golokB)
+print(round(osszesen/5, 1))
+
+
+print("3. feladat")
+
+gyozelmek= [0,0,0,0,0]
+for i in range(5):
+    if golokA[i]<golokB[i]:
+        gyozelmek[i]="V"
+    elif golokA[i]>golokB[i]:
+        gyozelmek[i]="H"
+    else:
+        gyozelmek[i]="D"
+
+print(gyozelmek)
+
+h=0
+v=0
+d=0
+
+for sor in gyozelmek:
+    if sor=="H":
+        h+=1
+    if sor=="V":
+        v+=1
+    if sor=="D":
+        d+=1
+
+if h>v:
+    print("A hazai csapat győzött többször.")
+elif h<v:
+    print("A vendég csapat győzött többször.")  
+else:
+    print("A két csapat ugyanannyiszor győzött.")  
+
+
+print("4. feladat")
+
+if d>0 :
+    print("Volt döntetlen mérkőzés.")
+else:
+    print("Nem volt döntetlen mérkőzés.")
+
