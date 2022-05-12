feda1d5fbac9c2bf99bfad7416f024f914d9f42b
baloghim <balogh.imre.mt@gmail.com>
2022-05-12 11:05:21 +0200

    Beolvasas

--- Szamvizsgalat.py
+++ Szamvizsgalat.py
@@ -1,2 +1,10 @@
-print(chr(27) + "[2J")
-print("Feri")
+lista = []
+
+while True:
+    x=int(input("Egész számokat kérek, kilépés 0-val. :"))
+    if x==0:
+        break
+    else:
+        lista.append(x)
+
+print(lista)
